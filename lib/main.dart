073183eab80344e5ca3ec97b1e27c26df0a64a97
Billy Stapleton <59696174+billy1479@users.noreply.github.com>
2023-07-12 17:30:31 +0100

    edits

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,85 @@ import 'package:movein/Pages/ScrollRefresh.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+class CustomCacheProvider extends CacheProvider {
+  @override
+  bool containsKey(String key, {String? defaultValue}) {
+    return Settings.getValue(key);
+  }
+
+  @override
+  bool? getBool(String key, {bool? defaultValue}) {
+    return Settings.getValue(key, defaultValue: true);
+  }
+
+  @override
+  double getDouble(String key, {double? defaultValue}) {
+    return Settings.getValue(key);
+  }
+
+  @override
+  int getInt(String key, {int? defaultValue}) {
+    return Settings.getValue(key);
+  }
+
+  @override
+  Set getKeys() {
+    throw UnimplementedError();
+  }
+
+  @override
+  String getString(String key, {String? defaultValue}) {
+    return Settings.getValue(key);
+  }
+
+  @override
+  T getValue<T>(String key, {T? defaultValue}) {
+    return Settings.getValue(key);
+  }
+
+  @override
+  Future<void> init() {
+    return Settings.init();
+  }
+
+  @override
+  Future<void> remove(String key, {Key? defaultValue}) {
+    Settings.getValue(key, defaultValue: 'hello');
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> removeAll() {
+    // Needs to be done
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> setBool(String key, bool? value) {
+    return Settings.setValue(key, value);
+  }
+
+  @override
+  Future<void> setDouble(String key, double? value) {
+    return Settings.setValue(key, value);
+  }
+
+  @override
+  Future<void> setInt(String key, int? value) {
+    return Settings.setValue(key, value);
+  }
+
+  @override
+  Future<void> setObject<T>(String key, T? value) {
+    return Settings.setValue(key, value);
+  }
+
+  @override
+  Future<void> setString(String key, String? value) {
+    return Settings.setValue(key, value);
+  }
+}
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,8 +98,9 @@ Future<void> main() async {
 
   // Run the app
   runApp(const App());
-
 }
+
+
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -31,7 +111,7 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: LAppTheme.lightTheme,
       darkTheme: LAppTheme.darkTheme,
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
 
       initialRoute: '/Scroller',
 
