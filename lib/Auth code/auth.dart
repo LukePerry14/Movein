b7fced3d1a3907c0434ac1ae1c6e98dca3f2ed67
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-10-10 17:43:05 +0100

    sendgrid start

--- lib/Auth code/auth.dart
+++ lib/Auth code/auth.dart
@@ -1,5 +1,45 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:http/http.dart' as http;
+
+// For Sendgrid mailing
+import 'package:sendgrid_mailer/sendgrid_mailer.dart';
+
+void sendEmail(to) async {
+  const String API_KEY =
+      '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';
+  const String TEMPLATE_ID = 'd-3d2e323131ea498d9e5e58348406a380';
+  const String SGEmail = 'feedback@move1n.co.uk';
+
+  final url = Uri.parse('https://api.sendgrid.com/v3/mail/send');
+  final headers = {
+    'Authorization': 'Bearer $API_KEY',
+    'Content-Type': 'application/json'
+  };
+
+  final body = {
+    "personalizations": [
+      {
+        'to': [
+          {'email': to}
+        ]
+      }
+    ],
+    "from": {'email': SGEmail},
+    "template_id": TEMPLATE_ID
+  };
+
+  final response =
+      await http.post(url, headers: headers, body: json.encode(body));
+
+  if (response.statusCode != 202) {
+    print('Email failed - ${response.body}');
+  } else {
+    print('email send');
+  }
+}
 
 class Auth {
   //Creating new instance of firebase auth
@@ -16,12 +56,15 @@ class Auth {
     return uid ?? "";
   }
 
-  addAccessToken(String accessToken, String userId)
-  {
-    FirebaseFirestore.instance.collection("Users").doc(userId).update({'AccessToken':accessToken})
-    .then((_){print('Success');})
-    .catchError((error)
-    {print('failed');});
+  addAccessToken(String accessToken, String userId) {
+    FirebaseFirestore.instance
+        .collection("Users")
+        .doc(userId)
+        .update({'AccessToken': accessToken}).then((_) {
+      print('Success');
+    }).catchError((error) {
+      print('failed');
+    });
   }
 
   Future<String> registerWithUserDetails(
